f5940527f398d2aaf1bb3cb29ffb03a1239db33f
Beverly Nelson <bevcnelson@gmail.com>
2013-07-21 05:05:16 -0500

    adds remote url

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,11 +2,15 @@ class Pin < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image, styles: { medium: "320x240>"}
 
-  attr_accessible :description, :image
+  attr_accessible :description, :image, :image_remote_url
   validates_presence_of :description, :user_id
   validates_attachment :image, presence: true,
                         content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
                         size: { less_than: 5.megabytes }
 
   
+  def image_remote_url=(url_value)
+    self.image = URI.parse(url_value) unless url_value.blank?
+    super
+  end
 end
