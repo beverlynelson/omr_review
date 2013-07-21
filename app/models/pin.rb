4df8e850150654c3ac1b24f1a694af8d586f63d1
Beverly Nelson <bevcnelson@gmail.com>
2013-07-21 03:39:17 -0500

    adds paperclip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,12 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description
-  validates_presence_of :description, :user_id
-
   belongs_to :user
+  has_attached_file :image, styles: { medium: "320x240>"}
 
+  attr_accessible :description, :image
+  validates_presence_of :description, :user_id
+  validates_attachment :image, presence: true,
+                        content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+                        size: { less_than: 5.megabytes }
+
+  
 end
